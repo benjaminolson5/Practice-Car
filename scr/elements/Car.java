e53ca3bfec7cd11b93a7730223cf9ec49cbbd812
Benjamin Olson <bolson8@wisc.edu>
2020-10-19 12:55:54 -0500

    Update Car.java

--- scr/elements/Car.java
+++ scr/elements/Car.java
@@ -1,9 +1,39 @@
+package elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Car {
 	
+	List<CarPart> carParts = new ArrayList<CarPart>();
 	
+	public Car () {
+		Brakes brake = new Brakes();
+		Battery battery = new Battery();
+		Axel axel = new Axel();
+		Alternator alternator = new Alternator();
+		Tire tire1 = new Tire();
+		Tire tire2 = new Tire();
+		Tire tire3 = new Tire();
+		Tire tire4 = new Tire();
+		Radiator radiator = new Radiator();
+		Engine engine = new Engine();
+		
+		carParts.add(brake);
+		carParts.add(battery);
+		carParts.add(axel);
+		carParts.add(alternator);
+		carParts.add(tire1);
+		carParts.add(tire2);
+		carParts.add(tire3);
+		carParts.add(tire4);
+		carParts.add(radiator);
+		carParts.add(engine);
+		
+	}
 
 	public void run () {
-		//This section will create instances of each CarPart, in order to construct the car during the simulation.
+		System.out.println("This is the Car run method.");
 	}
 	
 }
